52459a6725edc378a8ff36ae8b19e3373d78ac27
Alexander <152768619+a2p1k01@users.noreply.github.com>
2023-12-17 19:49:12 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from services.weather_api import get_weather
 
-app = FastAPI(title='Weather API')
+app = FastAPI(title='Weather API', version='1.0.2')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_heades=['*']
+)
 
 
 @app.get('/')
